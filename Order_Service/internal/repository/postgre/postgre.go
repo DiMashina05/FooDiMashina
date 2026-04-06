f881baad0c0392e29f7de6cf309d0e688ab7d86b
DiMashina05 <you@example.comzherlitsin.de@phystech.edu>
2026-04-07 00:46:42 +0300

    Update

--- Order_Service/internal/repository/postgre/postgre.go
+++ Order_Service/internal/repository/postgre/postgre.go
@@ -2,7 +2,10 @@ package postgre
 
 import "github.com/jackc/pgx/v5/pgxpool"
 
-type Postgre struct{
+type Postgre struct {
 	pool *pgxpool.Pool
 }
 
+func New(pool *pgxpool.Pool) *Postgre {
+	return &Postgre{pool: pool}
+}
